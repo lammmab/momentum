9c2de1428cd40d59f0286268485243eb3ab62662
lammmab <liamlam8282@gmail.com>
2026-09-09 13:44:52 -0400

    fixup PLATFORM checks with CR prefix

--- pc/src/core/boot/assets/asset_factory.cpp
+++ pc/src/core/boot/assets/asset_factory.cpp
@@ -1,12 +1,12 @@
 #include "momentum/boot/assets/asset_factory.hpp"
 
-#if defined(CR_PLATFORM_WINDOWS) || defined(CR_PLATFORM_POSIX)
+#if defined(PLATFORM_WINDOWS) || defined(PLATFORM_POSIX)
 #include "momentum/boot/assets/impl/asset_source_desktop.hpp"
 #endif
 
 namespace momentum::boot::assets {
     std::unique_ptr<IAssetSource> CreateAssetSource(momentum::platform::IPlatform& platform) {
-#if defined(CR_PLATFORM_WINDOWS) || defined(CR_PLATFORM_POSIX)
+#if defined(PLATFORM_WINDOWS) || defined(PLATFORM_POSIX)
         return std::make_unique<impl::AssetSourceDesktop>(platform);
 #else // Later we can use this to add support for other platforms, like consoles or mobile that dont use the typical desktop asset source
 #error "[src/core/boot/assets/asset_factory.cpp] No asset source implementation selected"
--- pc/src/platform/platform_factory.cpp
+++ pc/src/platform/platform_factory.cpp
@@ -1,16 +1,16 @@
 #include "momentum/platform/platform_factory.hpp"
 
-#if defined(CR_PLATFORM_WINDOWS)
+#if defined(PLATFORM_WINDOWS)
 #include "momentum/platform/impl/platform_windows.hpp"
-#elif defined(CR_PLATFORM_POSIX)
+#elif defined(PLATFORM_POSIX)
 #include "momentum/platform/impl/platform_posix.hpp"
 #endif
 
 namespace momentum::platform {
     std::unique_ptr<momentum::platform::IPlatform> CreatePlatform() {
-#if defined(CR_PLATFORM_WINDOWS)
+#if defined(PLATFORM_WINDOWS)
         return std::make_unique<momentum::platform::impl::PlatformWindows>();
-#elif defined(CR_PLATFORM_POSIX)
+#elif defined(PLATFORM_POSIX)
         return std::make_unique<momentum::platform::impl::PlatformPosix>();
 #else
 #error "[src/platform/platform_factory.cpp] No platform implementation selected"
